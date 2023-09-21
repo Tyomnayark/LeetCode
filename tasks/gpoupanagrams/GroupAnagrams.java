fa76a850f12a770993a822720bcd355f60226e61
tyomnayark <tyomnayark@gmail.com>
2023-09-21 08:42:42 +0600

    group angram

--- tasks/gpoupanagrams/GroupAnagrams.java
+++ tasks/gpoupanagrams/GroupAnagrams.java
@@ -1,9 +1,23 @@
 package tasks.gpoupanagrams;
 
-import java.util.List;
+import java.util.*;
 
 public class GroupAnagrams {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
     }
+    static public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagramMap = new HashMap<>();
+
+        for (String str : strs) {
+            byte[] byteArray = str.getBytes();
+            Arrays.sort(byteArray);
+            String sortedStr = new String(byteArray);
+
+            anagramMap.computeIfAbsent(sortedStr, k -> new  ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(anagramMap.values());
+    }
+
 }
