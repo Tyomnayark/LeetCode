86f886bcccb7f2e21d4b92655a00c0887ad740a3
Elena Tolmacheva <urfi-urfi@yandex.ru>
2023-09-27 14:46:21 +0600

    find occurence

--- tasks/occurancestring/FindOccuranceString.java
+++ tasks/occurancestring/FindOccuranceString.java
@@ -2,31 +2,6 @@ package tasks.occurancestring;
 
 public class FindOccuranceString {
     public int strStr(String haystack, String needle) {
-        if (haystack.length() < needle.length()){
-            return -1;
-        }
-
-        byte[] arrayHaystack = haystack.getBytes();
-        byte[] arrayNeedle = needle.getBytes();
-
-
-        for (int i = 0; i < arrayHaystack.length; i++ ){
-            if (arrayHaystack[i] == arrayNeedle[0]){
-                if (checkWord(i, arrayHaystack,arrayNeedle)){
-                    return i;
-                }
-            }
-        }
-        return -1;
-    }
-    public static boolean checkWord(int u, byte[] haystack, byte[] needle){
-        int res = 0;
-        for (int i = 0; i <  needle.length; i++){
-            if (u < haystack.length && needle[i] == haystack[u] ){
-                res++;
-            }
-            u++;
-        }
-        return   res == needle.length  ? true: false;
+        return haystack.indexOf(needle);
     }
 }
