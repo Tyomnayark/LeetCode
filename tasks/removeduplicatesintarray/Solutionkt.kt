12728f35d08762071dc0a66f43477821538e3ee5
Elena <tyomnayark@gmail.com>
2023-12-24 17:08:36 +0600

    remove duplicates

---
+++ tasks/removeduplicatesintarray/Solutionkt.kt
@@ -0,0 +1,19 @@
+package tasks.removeduplicatesintarray
+
+class Solutionkt {
+    fun removeDuplicates(nums: IntArray): Int {
+        if (nums.size <= 2) {
+            return nums.size
+        }
+
+        var k = 2
+
+        for (i in 2 until nums.size) {
+            if (nums[i] != nums[k - 1] || nums[i] != nums[k - 2]) {
+                nums[k++] = nums[i]
+            }
+        }
+
+        return k
+    }
+}
